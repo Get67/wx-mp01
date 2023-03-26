132314da08991244f1aa6f8196e2899595752812
Get67 <a1099668033@qq.com>
2023-03-26 15:13:53 +0800

    完成-2023年3月26日15:13:51

--- uni-shop-2/uni-shop-2/main.js
+++ uni-shop-2/uni-shop-2/main.js
@@ -1,44 +1,51 @@
 import Vue from 'vue'
 import App from './App'
+import store from '@/store/store.js'
 
-//导入网络请求的包
-import {
-  $http
-} from '@escook/request-miniprogram'
-// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
 uni.$http = $http
 
-//请求根路径
+// 请求的根路径
 $http.baseUrl = 'https://www.uinav.com'
 
-
-// 请求开始之前做一些事情   请求拦截器
+// 请求拦截器
 $http.beforeRequest = function(options) {
   uni.showLoading({
     title: '数据加载中...'
   })
+
+  // console.log(store)
+
+  // 判断当前请求的是否为有权限的接口
+  if (options.url.indexOf('/my/') !== -1) {
+    options.header = {
+      Authorization: store.state.m_user.token
+    }
+  }
 }
-// 请求完成之后做一些事情     响应拦截器
+
+// 响应拦截器
 $http.afterRequest = function() {
   uni.hideLoading()
 }
 
-// 封装的展示消息提示的方法
-uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+// 封装弹框的方法
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500) {
   uni.showToast({
     title,
     duration,
-    icon: 'none',
+    icon: 'none'
   })
 }
-
-
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-  ...App
+  ...App,
+  store
 })
 app.$mount()
